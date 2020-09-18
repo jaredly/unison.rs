c4947d55439cafa087eaa79359c2f22a9c5f8883
Jared Forsyth <jared@jaredforsyth.com>
2020-09-17 21:51:59 -0500

    working but no fall through

--- bin/src/server.rs
+++ bin/src/server.rs
@@ -1,11 +1,20 @@
 use futures::{FutureExt, StreamExt};
 use tokio::sync::{mpsc, watch, RwLock};
 // use std::future::Future;
+use rust_embed::RustEmbed;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use warp::filters::ws::{Message, WebSocket};
 use warp::Filter;
+
+#[derive(RustEmbed)]
+#[folder = "static"]
+struct Asset;
+
+#[derive(RustEmbed)]
+#[folder = "../wasm/pkg"]
+struct WasmPkg;
 
 type Receiver = watch::Receiver<Message>;
 
@@ -36,11 +45,9 @@ impl Pool {
     }
 }
 
-// type Users = Arc<RwLock<HashMap<usize, mpsc::UnboundedSender<Result<Message, warp::Error>>>>>;
 type PoolRef = Arc<RwLock<Pool>>;
 
 async fn add(pool: PoolRef, ws: WebSocket) {
-    // Split the socket into a sender and receive of messages.
     let (user_ws_tx, mut user_ws_rx) = ws.split();
 
     // Use an unbounded channel to handle buffering and flushing of messages
@@ -62,7 +69,44 @@ async fn add(pool: PoolRef, ws: WebSocket) {
     pool.write().await.drop(id);
 }
 
-async fn main() {
+enum Embed {
+    Success(&'static str, Vec<u8>),
+    Failure,
+}
+
+impl warp::Reply for Embed {
+    fn into_response(self) -> warp::reply::Response {
+        use http::{Response, StatusCode};
+        match self {
+            Embed::Success(mime, body) => Response::builder()
+                .header("Content-type", mime)
+                .status(StatusCode::OK)
+                .body(body.into())
+                .unwrap(),
+            Embed::Failure => {
+                let mut res = http::Response::default();
+                *res.status_mut() = StatusCode::NOT_FOUND;
+                res
+            }
+        }
+    }
+}
+
+fn mime_for_ext(f: &str) -> &'static str {
+    if f.ends_with(".js") {
+        "text/javascript"
+    } else if f.ends_with(".json") {
+        "application/json"
+    } else if f.ends_with(".html") {
+        "text/html"
+    } else if f.ends_with(".css") {
+        "text/css"
+    } else {
+        "application/octet-stream"
+    }
+}
+
+async fn main(path: &str) {
     let pool = PoolRef::default();
     let pool = warp::any().map(move || pool.clone());
 
@@ -75,6 +119,32 @@ async fn main() {
             });
 
     let fs = warp::fs::dir(".");
+    let wasm_assets =
+        warp::get().and(warp::path::tail()).map(
+            |tail: warp::filters::path::Tail| match WasmPkg::get(tail.as_str()) {
+                None => Embed::Failure,
+                Some(value) => Embed::Success(mime_for_ext(tail.as_str()), value.into_owned()),
+            },
+        );
 
-    warp::serve(ws.or(fs)).run(([127, 0, 0, 1], 3030)).await
+    let other_assets = warp::get().and(warp::path::tail()).map(
+        |tail: warp::filters::path::Tail| match Asset::get(tail.as_str()) {
+            None => Embed::Failure,
+            Some(value) => Embed::Success(mime_for_ext(tail.as_str()), value.into_owned()),
+        },
+    );
+
+    let res = ws.or(fs).or(wasm_assets).or(other_assets);
+
+    println!("Ok folks");
+    warp::serve(res).run(([127, 0, 0, 1], 3030)).await
+}
+
+pub fn serve(path: &str) -> std::io::Result<()> {
+    println!("0");
+    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    println!("A");
+    rt.block_on(main(path));
+    println!("B");
+    Ok(())
 }
