b8cdddd39f36d37e2c3f2e7bda48333956543c56
Jared Forsyth <jared@jaredforsyth.com>
2020-10-13 23:31:50 -0500

    oops forgot to add

---
+++ js/node.test.js
@@ -0,0 +1,32 @@
+import { RuntimeEnv, State, eval_value } from './ir_runtime';
+// import chalk from 'chalk';
+import fs from 'fs';
+
+const cmp = (a, b) => (a < b ? -1 : a > b ? 1 : 0);
+
+const data = JSON.parse(
+    fs.readFileSync(__dirname + '/../runtime_tests.json', 'utf8'),
+);
+const names = JSON.parse(
+    fs.readFileSync(__dirname + '/../runtime_tests.json.names.json', 'utf8'),
+);
+
+const debug = {};
+
+const env = new RuntimeEnv(data, names);
+
+const tests = Object.keys(names[0])
+    .filter(
+        (hash) =>
+            names[0][hash][0][0] === 'runtime_tests' &&
+            names[0][hash][0].slice(-1)[0].startsWith('t_'),
+    )
+    .sort((a, b) => cmp(names[0][a][0].join('.'), names[0][b][0].join('.')));
+
+tests.forEach((hash) => {
+    const name = names[0][hash][0].join('.');
+    test(name, () => {
+        const res = eval_value(env, hash, debug);
+        expect(res).toEqual({ Boolean: true });
+    });
+});
