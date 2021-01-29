a5e2d7c3fdd692ce4bf289061a940445d3e8563b
Jared Forsyth <jared@jaredforsyth.com>
2021-01-29 08:09:07 -0600

    ok learning about suffixes

--- bin/src/env.rs
+++ bin/src/env.rs
@@ -12,6 +12,11 @@ pub enum Error {
     NotImplemented(String),
 }
 
+fn concat(mut buf: std::path::PathBuf, thing: String) -> std::path::PathBuf {
+    buf.push(thing);
+    return buf;
+}
+
 #[derive(Clone, Debug)]
 pub struct Env {
     // stack: Vec<Frame>,
@@ -55,22 +60,36 @@ impl Env {
         match self.term_cache.get(hash) {
             Some(v) => Ok(v.clone()),
             None => {
-                let mut full = self.root.clone();
-                full.push("terms");
-                full.push("#".to_owned() + hash);
-                full.push("compiled.ub");
-                info!("Trying to load term {:?}", full);
-                let file = full.as_path();
+                let mut base = self.root.clone();
+                base.push("terms");
+                base.push("#".to_owned() + hash);
+                // full.push("compiled.ub");
+                info!("Trying to load term {:?}", base);
+                let mut f = concat(base.clone(), "compiled.ub".into());
+                let mut file = f.as_path();
                 if !file.exists() {
-                    unreachable!("Nope");
-                    // return Err(Error::TermNotFound(hash.to_owned()));
+                    // yay wonky hack
+                    // full = self.root.clone();
+                    // full.push("terms");
+                    // full.push("compiled.ub");
+                    base.pop();
+                    base.push(format!("#{}.0c3", hash));
+
+                    f = concat(base.clone(), "compiled.ub".into());
+                    file = f.as_path();
+                    if !file.exists() {
+                        println!("Didnt work also {:?}", file);
+                        // unreachable!("Nope");
+                        return Err(Error::TermNotFound(hash.to_owned()));
+                    }
                 }
                 let type_file = {
-                    let mut full = self.root.clone();
-                    full.push("terms");
-                    full.push("#".to_owned() + hash);
-                    full.push("type.ub");
-                    full
+                    // let mut full = self.root.clone();
+                    // full.push("terms");
+                    // full.push("#".to_owned() + hash);
+                    // full.push("type.ub");
+                    // full
+                    concat(base.clone(), "type.ub".into())
                 };
                 let typ = parser::Buffer::from_file(type_file.as_path())
                     .unwrap()
